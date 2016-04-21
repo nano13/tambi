cf48edffab1441be2c3840973e34ca336e0d2035
nano13 <nano13@gmx.net>
2016-04-21 22:17:26 +0200

    implemented draw, rubber, cursors, but slow

--- experiments/qgraphicsview_qt5.py
+++ experiments/qgraphicsview_qt5.py
@@ -1,6 +1,6 @@
 
 
-from PyQt5.QtWidgets import QWidget, QGraphicsView, QApplication, QGraphicsScene, QPushButton, QVBoxLayout, QGraphicsEllipseItem, QGraphicsLineItem
+from PyQt5.QtWidgets import QWidget, QGraphicsView, QApplication, QGraphicsScene, QPushButton, QVBoxLayout, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsRectItem
 from PyQt5 import QtCore
 from PyQt5.QtGui import QPen
 
@@ -23,46 +23,119 @@ class View(QGraphicsView):
         self.setScene(QGraphicsScene(self))
         self.setSceneRect(QtCore.QRectF(self.viewport().rect()))
         
+        self.penRadius = 5
+        self.rubberRadius = 10
+        
+        self.mouseButton = None
         self.lastMousePos = None
+        
+        self.mouseCursorShape = None
+        self.mouseCursorShapes = []
 
     def mousePressEvent(self, event):
-        self._start = event.pos()
-        self.lastMousePos = event.pos()
         
-        radius = 10
-        
-        pos = QtCore.QPointF(self.mapToScene(event.pos()))
-        ellipseItem = QGraphicsEllipseItem(pos.x()-(radius/2), pos.y()-(radius/2), radius, radius)
-        ellipseItem.setPen(QPen(QtCore.Qt.green, QtCore.Qt.SolidPattern))
-        ellipseItem.setBrush(QtCore.Qt.green)
-        
-        self.scene().addItem(ellipseItem)
-        
-        
-
-    def NOmouseReleaseEvent(self, event):
-        start = QtCore.QPointF(self.mapToScene(self._start))
-        end = QtCore.QPointF(self.mapToScene(event.pos()))
-        self.scene().addItem(
-            QtGui.QGraphicsLineItem(QtCore.QLineF(start, end)))
-        for point in (start, end):
-            text = self.scene().addSimpleText(
-                '(%d, %d)' % (point.x(), point.y()))
-            text.setBrush(QtCore.Qt.red)
-            text.setPos(point)
+        if event.button() == QtCore.Qt.LeftButton:
+            self.mouseButton = QtCore.Qt.LeftButton
             
+            self._start = event.pos()
+            self.lastMousePos = event.pos()
+            
+            pos = QtCore.QPointF(self.mapToScene(event.pos()))
+            ellipseItem = QGraphicsEllipseItem(pos.x()-(self.penRadius/2), pos.y()-(self.penRadius/2), self.penRadius, self.penRadius)
+            ellipseItem.setPen(QPen(QtCore.Qt.green, QtCore.Qt.SolidPattern))
+            ellipseItem.setBrush(QtCore.Qt.green)
+            
+            self.scene().addItem(ellipseItem)
+            
+        elif event.button() == QtCore.Qt.RightButton:
+            self.mouseButton = QtCore.Qt.RightButton
+            
+        self.updateMouseCursor(event)
+            
+    def updateMouseCursor(self, event):
+        
+        # remove cursors reliably after fast movements:
+        for item in self.mouseCursorShapes:
+            self.scene().removeItem(item)
+        
+        if self.mouseButton == QtCore.Qt.LeftButton:
+            x = event.pos().x()
+            y = event.pos().y()
+            pos = QtCore.QPointF(self.mapToScene(x, y))
+            
+            self.mouseCursorShape = QGraphicsEllipseItem(pos.x()-(self.penRadius/2), pos.y()-(self.penRadius/2), self.penRadius, self.penRadius)
+            self.mouseCursorShape.setPen(QPen(QtCore.Qt.red))
+            self.mouseCursorShape.setBrush(QtCore.Qt.yellow)
+            
+            if self.mouseCursorShape:
+                self.scene().removeItem(self.mouseCursorShape)
+            
+            self.scene().addItem(self.mouseCursorShape)
+            self.mouseCursorShapes.append(self.mouseCursorShape)
+        
+        elif self.mouseButton == QtCore.Qt.RightButton:
+            
+            x = event.pos().x() - self.rubberRadius/2
+            y = event.pos().y() - self.rubberRadius/2
+            pos = QtCore.QPointF(self.mapToScene(x, y))
+            
+            self.mouseCursorShape = QGraphicsRectItem(QtCore.QRectF(pos.x(), pos.y(), self.rubberRadius, self.rubberRadius))
+            self.mouseCursorShape.setPen(QPen(QtCore.Qt.red))
+            self.mouseCursorShape.setBrush(QtCore.Qt.yellow)
+            
+            if self.mouseCursorShape:
+                self.scene().removeItem(self.mouseCursorShape)
+                
+            self.scene().addItem(self.mouseCursorShape)
+            
+            self.mouseCursorShapes.append(self.mouseCursorShape)
+            
+            
+            
+    def mouseReleaseEvent(self, event):
+        if self.mouseCursorShape:
+            self.scene().removeItem(self.mouseCursorShape)
+        
+#    def NOmouseReleaseEvent(self, event):
+#        start = QtCore.QPointF(self.mapToScene(self._start))
+#        end = QtCore.QPointF(self.mapToScene(event.pos()))
+#        self.scene().addItem(
+#            QtGui.QGraphicsLineItem(QtCore.QLineF(start, end)))
+#        for point in (start, end):
+#            text = self.scene().addSimpleText(
+#                '(%d, %d)' % (point.x(), point.y()))
+#            text.setBrush(QtCore.Qt.red)
+#            text.setPos(point)
+    
     def mouseMoveEvent(self, event):
-        print(event.pos())
         
-        start = QtCore.QPointF(self.mapToScene(self.lastMousePos))
-        end = QtCore.QPointF(self.mapToScene(event.pos()))
+        self.updateMouseCursor(event)
         
-        lineItem = QGraphicsLineItem(QtCore.QLineF(start, end))
-        lineItem.setPen(QPen(QtCore.Qt.green, 10, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
-        
-        self.scene().addItem(lineItem)
-        
-        self.lastMousePos = event.pos()
+        if self.mouseButton == QtCore.Qt.LeftButton:
+            start = QtCore.QPointF(self.mapToScene(self.lastMousePos))
+            end = QtCore.QPointF(self.mapToScene(event.pos()))
+            
+            lineItem = QGraphicsLineItem(QtCore.QLineF(start, end))
+            lineItem.setPen(QPen(QtCore.Qt.green, self.penRadius, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+            self.scene().addItem(lineItem)
+            
+            self.lastMousePos = event.pos()
+            
+        elif self.mouseButton == QtCore.Qt.RightButton:
+            x = event.pos().x() - self.rubberRadius/2
+            y = event.pos().y() - self.rubberRadius/2
+            pos = QtCore.QPointF(self.mapToScene(x, y))
+            rect = QtCore.QRectF(pos.x(), pos.y(), self.rubberRadius, self.rubberRadius)
+            items = self.scene().items(rect, QtCore.Qt.IntersectsItemShape)
+            if items:
+                for item in items:
+                    if not item == self.mouseCursorShape:
+                        self.scene().removeItem(item)
+            
+#            pos = QtCore.QPointF(self.mapToScene(event.pos()))
+#            item = self.scene().itemAt(pos, self.transform())
+#            if item:
+#                self.scene().removeItem(item)
         
 if __name__ == '__main__':
 
