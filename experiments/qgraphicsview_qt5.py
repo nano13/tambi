72604a1be63d1a4f11c4f1f7fbb9c3d0b76f82d8
nano13 <nano13@gmx.net>
2016-04-22 00:52:35 +0200

    cursor durch manipulation von qcursor optimiert

--- experiments/qgraphicsview_qt5.py
+++ experiments/qgraphicsview_qt5.py
@@ -1,8 +1,8 @@
 
 
-from PyQt5.QtWidgets import QWidget, QGraphicsView, QApplication, QGraphicsScene, QPushButton, QVBoxLayout, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsRectItem
+from PyQt5.QtWidgets import QWidget, QGraphicsView, QApplication, QGraphicsScene, QPushButton, QVBoxLayout, QGraphicsItem, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsRectItem
 from PyQt5 import QtCore
-from PyQt5.QtGui import QPen
+from PyQt5.QtGui import QPen, QCursor, QPixmap
 
 class Window(QWidget):
     def __init__(self):
@@ -28,9 +28,6 @@ class View(QGraphicsView):
         
         self.mouseButton = None
         self.lastMousePos = None
-        
-        self.mouseCursorShape = None
-        self.mouseCursorShapes = []
 
     def mousePressEvent(self, event):
         
@@ -50,51 +47,26 @@ class View(QGraphicsView):
         elif event.button() == QtCore.Qt.RightButton:
             self.mouseButton = QtCore.Qt.RightButton
             
+            self.eraseWithRubber(event)
+            
         self.updateMouseCursor(event)
             
     def updateMouseCursor(self, event):
         
-        # remove cursors reliably after fast movements:
-        for item in self.mouseCursorShapes:
-            self.scene().removeItem(item)
-        
         if self.mouseButton == QtCore.Qt.LeftButton:
-            x = event.pos().x()
-            y = event.pos().y()
-            pos = QtCore.QPointF(self.mapToScene(x, y))
-            
-            self.mouseCursorShape = QGraphicsEllipseItem(pos.x()-(self.penRadius/2), pos.y()-(self.penRadius/2), self.penRadius, self.penRadius)
-            self.mouseCursorShape.setPen(QPen(QtCore.Qt.red))
-            self.mouseCursorShape.setBrush(QtCore.Qt.yellow)
-            
-            if self.mouseCursorShape:
-                self.scene().removeItem(self.mouseCursorShape)
-            
-            self.scene().addItem(self.mouseCursorShape)
-            self.mouseCursorShapes.append(self.mouseCursorShape)
+            #QApplication.setOverrideCursor(QCursor(QtCore.Qt.CrossCursor))
+            cursor_pixmap = QPixmap("red_cursor.png")
+            cursor_pixmap = cursor_pixmap.scaled(self.penRadius, self.penRadius, QtCore.Qt.KeepAspectRatio)
+            QApplication.setOverrideCursor(QCursor(cursor_pixmap))
         
         elif self.mouseButton == QtCore.Qt.RightButton:
-            
-            x = event.pos().x() - self.rubberRadius/2
-            y = event.pos().y() - self.rubberRadius/2
-            pos = QtCore.QPointF(self.mapToScene(x, y))
-            
-            self.mouseCursorShape = QGraphicsRectItem(QtCore.QRectF(pos.x(), pos.y(), self.rubberRadius, self.rubberRadius))
-            self.mouseCursorShape.setPen(QPen(QtCore.Qt.red))
-            self.mouseCursorShape.setBrush(QtCore.Qt.yellow)
-            
-            if self.mouseCursorShape:
-                self.scene().removeItem(self.mouseCursorShape)
-                
-            self.scene().addItem(self.mouseCursorShape)
-            
-            self.mouseCursorShapes.append(self.mouseCursorShape)
-            
-            
+            #QApplication.setOverrideCursor(QCursor(QtCore.Qt.CrossCursor))
+            cursor_pixmap = QPixmap("red_cursor.png")
+            cursor_pixmap = cursor_pixmap.scaled(self.rubberRadius, self.rubberRadius, QtCore.Qt.KeepAspectRatio)
+            QApplication.setOverrideCursor(QCursor(cursor_pixmap))
             
     def mouseReleaseEvent(self, event):
-        if self.mouseCursorShape:
-            self.scene().removeItem(self.mouseCursorShape)
+        QApplication.restoreOverrideCursor()
         
 #    def NOmouseReleaseEvent(self, event):
 #        start = QtCore.QPointF(self.mapToScene(self._start))
@@ -109,8 +81,6 @@ class View(QGraphicsView):
     
     def mouseMoveEvent(self, event):
         
-        self.updateMouseCursor(event)
-        
         if self.mouseButton == QtCore.Qt.LeftButton:
             start = QtCore.QPointF(self.mapToScene(self.lastMousePos))
             end = QtCore.QPointF(self.mapToScene(event.pos()))
@@ -122,20 +92,17 @@ class View(QGraphicsView):
             self.lastMousePos = event.pos()
             
         elif self.mouseButton == QtCore.Qt.RightButton:
-            x = event.pos().x() - self.rubberRadius/2
-            y = event.pos().y() - self.rubberRadius/2
-            pos = QtCore.QPointF(self.mapToScene(x, y))
-            rect = QtCore.QRectF(pos.x(), pos.y(), self.rubberRadius, self.rubberRadius)
-            items = self.scene().items(rect, QtCore.Qt.IntersectsItemShape)
-            if items:
-                for item in items:
-                    if not item == self.mouseCursorShape:
-                        self.scene().removeItem(item)
+            self.eraseWithRubber(event)
             
-#            pos = QtCore.QPointF(self.mapToScene(event.pos()))
-#            item = self.scene().itemAt(pos, self.transform())
-#            if item:
-#                self.scene().removeItem(item)
+    def eraseWithRubber(self, event):
+        x = event.pos().x() - self.rubberRadius/2
+        y = event.pos().y() - self.rubberRadius/2
+        pos = QtCore.QPointF(self.mapToScene(x, y))
+        rect = QtCore.QRectF(pos.x(), pos.y(), self.rubberRadius, self.rubberRadius)
+        items = self.scene().items(rect, QtCore.Qt.IntersectsItemShape)
+        if items:
+            for item in items:
+                self.scene().removeItem(item)
         
 if __name__ == '__main__':
 
