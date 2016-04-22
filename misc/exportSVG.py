4aad36489e69dc68bb7561d5e65a158d5d9dc407
nano13 <nano13@gmx.net>
2016-04-22 03:01:23 +0200

    i

--- misc/exportSVG.py
+++ misc/exportSVG.py
@@ -3,3 +3,19 @@
 class ExportSVG(object):
     def __init__(self):
         pass
+    
+    def startExport(self):
+        self.fobj = open("experiments/out.svg", "w")
+        self.header()
+        
+        self.fobj.close()
+    
+    def header(self):
+        self.fobj.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        self.fobj.write('<svg xmlns="http://www.w3.org/2000/svg"\n')
+        self.fobj.write('\tversion="1.1" baseProfile="full"\n')
+        self.fobj.write('\twidth="{0}px" height="{1}"px viewBox="{2} {3} {4} {5}">\n'.format())
+        
+        
+    def footer(self):
+        self.fobj.write('</svg>')
