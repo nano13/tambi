ca6fe55128d304ced957b748473ab6339676aba1
nano13 <nano13@gmx.net>
2017-02-18 00:18:28 +0100

    wordlist top 5000 en_US

---
+++ modules/arabic/wordfrequency/wordlist2sqlite.py
@@ -0,0 +1,16 @@
+
+class Wordlist2Sqlite(object):
+    def __init__(self):
+        self.iterate()
+    
+    def iterate(self):
+        fobj = open("./wordlist_5000.csv", "r")
+        
+        for line in fobj:
+            rank, word, part, frequency, dispersion = line.split("|")
+            
+        
+        fobj.close()
+    
+if __name__ == '__main__':
+    c = Wordlist2Sqlite()
