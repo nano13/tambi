8ae54a27ca1621cd526779b9c4f390e061c692aa
nano13 <nano13@gmx.net>
2017-03-08 03:35:56 +0100

    man-page for module bituza initiated

---
+++ modules/bituza/man.py
@@ -0,0 +1,29 @@
+
+module_description = """
+Module for accessing a database for numerological analysis of the bible. This database was created by Paul Gerhard Zint from http://www.zeitundzahl.de/
+
+Particularly interesting could be the also included interlinear translation between the original languages (hebrew, aramaic, greek) and the germen 'Elberfelder' translation. This can be accessed by the command bituza.word
+"""
+
+books = """
+Returns a list of books as to be used with the command bituza.word
+"""
+
+search_unicode = """
+Search the given pattern in the original language.
+
+@param (required): [string], containing the search-pattern.
+"""
+
+word = """
+Returns the specified bible passage with an interlinear view and all numerological data.
+
+@param (required): [string], the name of the book we want to read. See the command bituza.books.
+@param (required): [number], the chapter of the book we want to read.
+@param (optional): [number] or [number]-[number], the verse or a range of verses to be shown.
+
+Examples:
+bituza.word 1mose 1
+bituza.word 1mose 1 1
+bituza.word 1mose 1 10-20
+"""
