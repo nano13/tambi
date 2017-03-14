53bdf4d34091f28dc2d854717f3b849dca6c2879
nano13 <nano13@gmx.net>
2017-03-14 13:33:33 +0100

    font "Scheherazade" for arabic implemented

--- misc/detectUnicodeBlock.py
+++ misc/detectUnicodeBlock.py
@@ -1,4 +1,6 @@
 
+from PyQt5.QtGui import QFontDatabase, QFont
+
 def isArabic(string):
     #return all(ord(char) > 1536 and ord(char) < 1791 for char in string)
     result = False
@@ -15,6 +17,24 @@ def isHebrew(string):
             result = True
     return result 
 
+def applyCoranicaFontToQWidget(widget):
+    #QFontDatabase.addApplicationFont("./assets/fonts/coranica_1145.ttf")
+    #font = QFont("coranica")
+    QFontDatabase.addApplicationFont("./assets/fonts/Scheherazade-2.100/Scheherazade-Regular.ttf")
+    font = QFont("Scheherazade")
+    widget.setFont(font)
+    
+    #db = QFontDatabase()
+    #fonts = QFontDatabase.families(db)
+    #for font in fonts:
+        #if font.find("ehera"):
+            #print(font)
+
 def applyFontSizeToQWidget(string, widget):
-    if isArabic(string) or isHebrew(string):
+    if isArabic(string):
+        applyCoranicaFontToQWidget(widget)
+        widget.setFontPointSize(40)
+    
+    if isHebrew(string):
         widget.setFontPointSize(20)
+
