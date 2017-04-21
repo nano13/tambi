1e38a872667c555d61753bbe9e30442808e2b0fe
nano13 <nano13@gmx.net>
2017-04-21 18:45:23 +0200

    pulled out amazing grace to own class

---
+++ QCustomizedWidgets/QAmazingGraceWidget.py
@@ -0,0 +1,52 @@
+
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton
+
+from QCustomizedWidgets.QBeamerWindow import QBeamerWindow
+
+from functools import partial
+
+class QAmazingGraceWidget(QWidget):
+    
+    beamer = None
+    
+    def __init__(self):
+        super().__init__()
+        
+        grid = QGridLayout()
+        self.setLayout(grid)
+        
+        import os
+        base, dirs, files = next(iter(os.walk('./amazing_grace')))
+        max_i = 0
+        for i, f in enumerate(sorted(files)):
+            label = str(f.split('.')[0])
+            button = QPushButton(label)
+            if i < len(files)/2:
+                grid.addWidget(button, i, 0)
+            else:
+                grid.addWidget(button, i-len(files)/2, 1)
+            button.clicked.connect(partial(self.amazingGraceButtonClicked, label))
+            max_i += 1
+            
+        blank = QPushButton('blank')
+        grid.addWidget(blank, max_i, 0, 1, 2)
+        blank.clicked.connect(self.beamerCanvasBlank)
+    
+    def amazingGraceButtonClicked(self, language):
+        if not self.beamer == None:
+            self.beamer.destroy()
+        
+        text = ''
+        with open('./amazing_grace/'+str(language)+'.tex', 'r') as fobj:
+            for line in fobj:
+                text += line.replace('\\\\', '')
+        
+        canvas = QBeamerWindow()
+        canvas.setText(text)
+        canvas.routeToScreen()
+        self.beamer = canvas
+        canvas.showFullScreen()
+        
+    def beamerCanvasBlank(self):
+        if not self.beamer == None:
+            self.beamer.destroy()
--- QCustomizedWidgets/QCoreTab.py
+++ QCustomizedWidgets/QCoreTab.py
@@ -4,14 +4,10 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QTextEdit
 from QCustomizedWidgets.QCliTab import QCliTab
 from QCustomizedWidgets.QVocableStackedWidget import QVocableStackedWidget
 from QCustomizedWidgets.QMusicBeamerWidget import QMusicBeamerWidget
-
-from functools import partial
-
-from QCustomizedWidgets.QBeamerWindow import QBeamerWindow
+from QCustomizedWidgets.QAmazingGraceWidget import QAmazingGraceWidget
 
 class QCoreTab(QWidget):
     
-    beamer = None
     
     def __init__(self):
         super().__init__()
@@ -42,41 +38,7 @@ class QCoreTab(QWidget):
         return music_beamer_widget
     
     def amazingGraceTab(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
-        
-        import os
-        base, dirs, files = next(iter(os.walk('./amazing_grace')))
-        max_i = 0
-        for i, f in enumerate(sorted(files)):
-            label = str(f.split('.')[0])
-            button = QPushButton(label)
-            if i < len(files)/2:
-                grid.addWidget(button, i, 0)
-            else:
-                grid.addWidget(button, i-len(files)/2, 1)
-            button.clicked.connect(partial(self.amazingGraceButtonClicked, label))
-            max_i += 1
-            
-        blank = QPushButton('blank')
-        grid.addWidget(blank, max_i, 0, 1, 2)
-        blank.clicked.connect(self.beamerCanvasBlank)
-        return self
-    def amazingGraceButtonClicked(self, language):
-        if not self.beamer == None:
-            self.beamer.destroy()
-        
-        text = ''
-        with open('./amazing_grace/'+str(language)+'.tex', 'r') as fobj:
-            for line in fobj:
-                text += line.replace('\\\\', '')
-        
-        canvas = QBeamerWindow()
-        canvas.setText(text)
-        canvas.routeToScreen()
-        self.beamer = canvas
-        canvas.showFullScreen()
-    def beamerCanvasBlank(self):
-        if not self.beamer == None:
-            self.beamer.destroy()
+        amazing = QAmazingGraceWidget()
+        return amazing
+    
     
