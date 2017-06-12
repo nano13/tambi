2b0b661fc25ed8a48b8e30646f035e3a7a0614d0
nano13 <nano13@gmx.net>
2017-06-12 19:13:40 +0200

    frets.commands implemented

--- modules/frets/frets.py
+++ modules/frets/frets.py
@@ -8,6 +8,8 @@ class Frets(object):
     
     def getCommands(self):
         return {
+                "frets.commands" : self.commands,
+                
                 "frets.capo" : self.capo,
                 "frets.custom" : self.frets,
                 "frets.guitar" : self.fretsGuitar,
@@ -22,6 +24,21 @@ class Frets(object):
     
     def commandNotFound(self, c, a):
         raise CommandNotInThisModule("command not found in module 'strings'")
+    
+    def commands(self, none1, none2):
+        dic = self.getCommands()
+        
+        commands = sorted(dic.items())
+        
+        all_commands = []
+        for key in commands:
+            line = str(key).split(",")[0]
+            all_commands.append(str(line[2:-1]))
+            
+        result_object = Result()
+        result_object.category = "list"
+        result_object.payload = all_commands
+        return result_object
     
     def fretsGuitar(self, c, a):
         return self.frets("frets", ["e", "a", "d", "g", "h", "e"])
