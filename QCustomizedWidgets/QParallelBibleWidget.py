0ca792306fbc2f410134468a5d1b17167db401d6
nano13 <nano13@gmx.net>
2017-07-11 19:13:25 +0200

    QParallelBibleWidget: first actually working version with three translations

--- QCustomizedWidgets/QParallelBibleWidget.py
+++ QCustomizedWidgets/QParallelBibleWidget.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QTextEdit, QComboBox
+from PyQt5.QtWidgets import QWidget, QLayout, QVBoxLayout, QGridLayout, QTextEdit, QComboBox
 
 from QCustomizedWidgets.QInputLine import QInputLine
 
@@ -10,10 +10,8 @@ class QParallelBibleWidget(QWidget):
     interpreter = Interpreter()
     
     current_sword_module = None
-    modules_list = ['GerNeUe', 'TR', 'GerSch']
+    #modules_list = ['GerNeUe', 'TR', 'GerSch']
     display_widgets_list = []
-    
-    modules_dropdown_list = []
     
     def __init__(self):
         super().__init__()
@@ -22,9 +20,12 @@ class QParallelBibleWidget(QWidget):
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.layout)
         
-        self.display_widget = QDisplayWidget()
-        self.display_widget.setDropdownItems(self.getModulesForDropdown())
-        self.layout.addWidget(self.display_widget)
+        modules_dropdown_list = self.getModulesForDropdown()
+        for i in range(0, 3):
+            display_widget = QDisplayWidget()
+            display_widget.setDropdownItems(modules_dropdown_list)
+            self.display_widgets_list.append(display_widget)
+            self.layout.addWidget(display_widget)
         
         line = QInputLine()
         line.return_pressed.connect(self.commandEntered)
@@ -32,22 +33,22 @@ class QParallelBibleWidget(QWidget):
         
     def commandEntered(self, command):
         """ store the originally selected sword module """
-        self.current_sword_module = self.interpreter.interpreter('sword.getModule')
+        self.current_sword_module = self.interpreter.interpreter('sword.getModule').payload
         
-        for i, module in enumerate(self.modules_list):
+        for i, widget in enumerate(self.display_widgets_list):
             
             try:
-                self.interpreter.interpreter('sword.setModule '+module)
+                self.interpreter.interpreter('sword.setModule '+widget.getModuleName())
                 
                 result = self.interpreter.interpreter(command)
             except ClearCalled:
                 self.clearDisplayWidget()
             else:
                 print(result.toString())
-                self.display_widgets_list[i].setText(result)
+                widget.setText(result.toString())
         
         """ restore the originally seletectd sword module """
-        self.interpreter.interpreter('sword.setModule '+module)
+        self.interpreter.interpreter('sword.setModule '+self.current_sword_module)
     
     def clearDisplayWidget(self):
         pass
@@ -55,8 +56,16 @@ class QParallelBibleWidget(QWidget):
     def getModulesForDropdown(self):
         result = self.interpreter.interpreter('sword.modules')
         
-        print(result.payload[0])
-        self.modules_dropdown_list = result
+        modules = []
+        for module in result.payload:
+            modules.append(module[0])
+        
+        return sorted(modules)
+    
+class QDisplayLayout(QLayout):
+    
+    def __init__(self):
+        super().__init__()
 
 class QDisplayWidget(QWidget):
     
@@ -68,10 +77,12 @@ class QDisplayWidget(QWidget):
         self.setLayout(self.layout)
         
         self.dropdown = QComboBox()
+        self.dropdown.currentIndexChanged.connect(self.comboBoxChanged)
         #self.dropdown.insertItems(0, ['a', 'b'])
         self.layout.addWidget(self.dropdown)
         
         self.display_widget = QTextEdit()
+        self.display_widget.setReadOnly(True)
         self.layout.addWidget(self.display_widget)
     
     def setDropdownItems(self, items):
@@ -79,4 +90,11 @@ class QDisplayWidget(QWidget):
     
     def setText(self, text):
         self.display_widget.setText(text)
+        
+    def getModuleName(self):
+        return self.dropdown.currentText()
+        
+    def comboBoxChanged(self, index):
+        print(index)
+        print(self.dropdown.currentText())
     
