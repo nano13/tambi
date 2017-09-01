d93c4d60d5683f57bd16c232896573a15e06f033
nano13 <nano13@gmx.net>
2017-09-01 22:26:42 +0200

    moved gps functionality to new module "gps"

--- interpreter/coreCommands.py
+++ interpreter/coreCommands.py
@@ -34,8 +34,6 @@ class CoreCommands(object):
             "history" : self.history,
             
             "fonts" : self.fonts,
-            
-            "position" : self.position,
             }
     
     def commandNotFound(self, command, args):
@@ -154,59 +152,3 @@ class CoreCommands(object):
         result_object.payload = available_fonts
         return result_object
     
-    def position(self, c, a):
-        result_object = Result()
-        result_object.payload = ""
-        try:
-            import gpsd
-        except ModuleNotFoundError:
-            from QCustomizedWidgets.QLocationManager import QLocationManager
-            location = QLocationManager()
-            pos = location.getGpsPosition()
-            
-            result_object.category = "text"
-            result_object.payload = "using qt-backend"
-        else:
-            try:
-                gpsd.connect()
-            except ConnectionRefusedError:
-                result_object.category = "text"
-                result_object.payload = "could not connect to gpsd"
-            else:
-                packet = gpsd.get_current()
-                
-                try:
-                    pos = packet.position()
-                except gpsd.NoFixError:
-                    result_object.category = 'text'
-                    result_object.payload = "gpsd: No Fix"
-                else:
-                    result_object.category = 'table'
-                    
-                    precision = packet.position_precision()
-                    time = packet.get_time()
-                    try:
-                        alt = packet.altitude()
-                        movement = packet.movement()
-                    except gpsd.NoFixError:
-                        alt = 'N.A.'
-                        speed, track, climb = 'N.A.', 'N.A.', 'N.A.'
-                    else:
-                        speed, track, climb = movement['speed'], movement['track'], movement['climb']
-                    
-                    result_object.payload = [
-                        ['Latitude', pos[0]],
-                        ['Longitude', pos[1]],
-                        ['Altitude', alt],
-                        
-                        ['Speed', speed],
-                        ['Track', track],
-                        ['Climb', climb],
-                        
-                        ['Time', time],
-                        
-                        ['Error Horizontal', precision[0]],
-                        ['Error Vertical', precision[1]],
-                    ]
-        
-        return result_object
