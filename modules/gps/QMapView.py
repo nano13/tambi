39218172a22b2754587e6d6585b4137dfa6dd9bf
nano13 <nano13@gmx.net>
2017-09-08 04:11:58 +0200

    qmapview: zoom in/out implemented caching implemented

--- modules/gps/QMapView.py
+++ modules/gps/QMapView.py
@@ -1,17 +1,21 @@
 
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGridLayout, QWidget, QGraphicsEllipseItem
-from PyQt5.QtGui import QPen, QPainter
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGridLayout, QWidget, QGraphicsEllipseItem, QPushButton
+from PyQt5.QtGui import QPen, QPainter, QIcon
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt, pyqtSignal, QThread, QRectF
 
+from configs.configFiles import ConfigFile
+
 import urllib.request
-import math, _thread
+import math, _thread, os
 
 class QMapView(QGraphicsView):
-    
-    zoom = 16
+
+    #zoom = 16
+    zoom = 10
     boundings_path = None
     bounds = None
+    download_thread = None
     corners = {
         'nw' : None,
         'sw' : None,
@@ -27,18 +31,66 @@ class QMapView(QGraphicsView):
     
     PEN_RADIUS = 4
     COLOUR = QtCore.Qt.darkGreen
-    
+
     def __init__(self, boundings):
         super().__init__()
         self.boundings_path = boundings
-        
+
         self.setScene(QGraphicsScene(self))
         self.setRenderHint(QPainter.Antialiasing)
         
-        self.calculateNeededTiles(boundings)
+        config = ConfigFile()
+        self.cache_path = config.readPath('cache', 'cachepath')
+        self.cache_path = os.path.join(self.cache_path, 'maps')
+        if not os.path.exists(self.cache_path):
+            os.makedirs(self.cache_path)
         
+        self.calculateNeededTiles(boundings)
+
         #self.bounds = self.scene().itemsBoundingRect()
+        
+        grid = QGridLayout()
+        grid.setRowStretch(0, 10000)
+        grid.setColumnStretch(0, 10000)
+        self.setLayout(grid)
+        
+        zoom_in_button = QPushButton()
+        zoom_in_button.setIcon(QIcon.fromTheme('zoom-in'))
+        zoom_in_button.setMaximumSize(25, 25)
+        grid.addWidget(zoom_in_button, 1, 1)
+        zoom_in_button.clicked.connect(self.zoomInClicked)
+        
+        zoom_out_button = QPushButton()
+        zoom_out_button.setIcon(QIcon.fromTheme('zoom-out'))
+        zoom_out_button.setMaximumSize(25, 25)
+        grid.addWidget(zoom_out_button, 2, 1)
+        zoom_out_button.clicked.connect(self.zoomOutClicked)
+        
+    def zoomInClicked(self):
+        if self.download_thread:
+            self.download_thread.stop()
+        
+        if self.zoom < 19:
+            self.zoom += 1
+        self.scene().clear()
+        
+        self.calculateNeededTiles(self.boundings_path)
     
+    def zoomOutClicked(self):
+        if self.download_thread:
+            self.download_thread.stop()
+        
+        if self.zoom > 0:
+            self.zoom -= 1
+        self.scene().clear()
+        self.items().clear()
+        
+        """ because this seems to be the best way to shrink the scene again: """
+        self.setScene(QGraphicsScene(self))
+        self.setRenderHint(QPainter.Antialiasing)
+        
+        self.calculateNeededTiles(self.boundings_path)
+
     def calculateNeededTiles(self, boundings):
         tile_min_x, tile_max_y = self.degToTileNumber(boundings['lat_min'], boundings['lon_min'])
         
@@ -50,7 +102,7 @@ class QMapView(QGraphicsView):
         self.bounds = QRectF(0, 0, (tile_max_x+1 - tile_min_x)*256, (tile_max_y+1 - tile_min_y)*256)
         print(self.bounds)
         
-        self.download_thread = QDownloadMapTilesThread(self.zoom, tile_min_x, tile_max_x+1, tile_min_y, tile_max_y+1)
+        self.download_thread = QDownloadMapTilesThread(self.cache_path, self.zoom, tile_min_x, tile_max_x+1, tile_min_y, tile_max_y+1)
         self.download_thread.drawMapTile.connect(self.drawMapTile)
         self.download_thread.start()
         
@@ -67,12 +119,10 @@ class QMapView(QGraphicsView):
         self.corners_mercator['sw'] = self.convertToWebMercator_(self.corners['sw'])
         self.corners_mercator['se'] = self.convertToWebMercator_(self.corners['se'])
     
-    
-    
     def drawMapTile(self, pixmap, pos_x, pos_y):
         tile_size = 256
         #pixmap = pixmap.scaled(tile_size, tile_size, QtCore.Qt.KeepAspectRatio)
-        
+
         item = self.scene().addPixmap(pixmap)
         item.setPos(pos_y*tile_size, pos_x*tile_size)
         item.setZValue(-10)
@@ -158,35 +208,51 @@ class QMapView(QGraphicsView):
     def scaleViewToContents(self):
         bounds = self.scene().itemsBoundingRect()
         self.fitInView(bounds, Qt.KeepAspectRatio)
-
+    
 
 class QDownloadMapTilesThread(QThread):
     
-    
+    __stop = False
     drawMapTile = pyqtSignal(object, float, float)
     
-    def __init__(self, zoom, x_min, x_max, y_min, y_max):
+    def __init__(self, cache_path, zoom, x_min, x_max, y_min, y_max):
         super().__init__()
         
+        self.cache_path = cache_path
         self.zoom = zoom
         self.x_min, self.x_max = x_min, x_max
         self.y_min, self.y_max = y_min, y_max
     
     def run(self):
         for i, x in enumerate(range(self.x_min, self.x_max)):
+            if self.__stop:
+                break
             #QApplication.processEvents()
             for j, y in enumerate(range(self.y_min, self.y_max)):
+                if self.__stop:
+                    break
                 #print(x, y, i, j)
                 #print(self.x_min, self.x_max, self.y_min, self.y_max)
                 self.fetchTile(x, y, j, i)
     
     def fetchTile(self, x, y, pos_x, pos_y):
-        tile = urllib.request.urlopen("http://a.tile.openstreetmap.org/{0}/{1}/{2}.png".format(self.zoom, x, y))
-        image = tile.read()
-        
         pixmap = QtGui.QPixmap()
-        pixmap.loadFromData(image)
+        
+        image_filename = os.path.join(self.cache_path, str(self.zoom)+'_'+str(x)+'_'+str(y)+'.png')
+        if not os.path.exists(image_filename):
+            tile = urllib.request.urlopen("http://a.tile.openstreetmap.org/{0}/{1}/{2}.png".format(self.zoom, x, y))
+            image = tile.read()
+            pixmap.loadFromData(image)
+            
+            with open(image_filename, 'bw') as _b:
+                _b.write(image)
+        else:
+            pixmap.load(image_filename)
+        
         self.drawMapTile.emit(pixmap, pos_x, pos_y)
+    
+    def stop(self):
+        self.__stop = True
 
 from PyQt5.QtWidgets import QMainWindow
 
@@ -206,7 +272,7 @@ class QMapWindow(QMainWindow):
 if __name__ == '__main__':
     import signal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    
+        
     from PyQt5.QtWidgets import QApplication
     import sys
     app = QApplication(sys.argv)
