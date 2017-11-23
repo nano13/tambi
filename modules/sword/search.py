d9d81d88b0cf7212713dd137c7820394b8e88773
nano13 <nano13@gmx.net>
2017-11-23 02:27:48 +0100

    initialized sword.search

--- modules/sword/search.py
+++ modules/sword/search.py
@@ -1,5 +1,11 @@
 
+from whoosh import index
+from whoosh.fields import Schema, ID, TEXT, KEYWORD, NUMERIC
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.searching import Searcher
+from whoosh.qparser import QueryParser, MultifieldParser
 
+import sqlite3
 
 class Search(object):
     
