96881728f002fe9c4deb6c571fd7aceb8028b340
nano13 <nano13@gmx.net>
2017-12-05 05:48:41 +0100

    using oxygen theme (which is included)

--- c++/tambi.cpp
+++ c++/tambi.cpp
@@ -18,6 +18,8 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName("tambi");
     
     MainWindow mainWin;
+    QIcon::setThemeSearchPaths(QStringList("../assets/icons"));
+    QIcon::setThemeName("oxygen");
     mainWin.setWindowIcon(QIcon("../assets/icons/logo2.png"));
     mainWin.show();
     return app.exec();
