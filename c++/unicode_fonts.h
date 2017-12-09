70d48792e949034a90748762d924be29ac1d4b3e
nano13 <nano13@gmx.net>
2017-12-09 15:46:48 +0100

    constants in unicode_fonts.h

--- c++/unicode_fonts.h
+++ c++/unicode_fonts.h
@@ -18,6 +18,22 @@ public:
     void applyFontToQWidget(QString, QWidget *widget);
     
     QString printFonts(QString);
+    
+private:
+    int arabic_block[2] = {1536, 1791};
+    int hebrew_block[2] = {1424, 1535};
+    int greek_block[2] = {880, 1023};
+    int ipa_block[2] = {250, 687};
+    
+    QString arabic_font = "Scheherazade";
+    QString hebrew_font = "Ezra SIL";
+    QString greek_font = "Galatia SIL";
+    QString ipa_font = "Doulos SIL";
+    
+    int arabic_size = 40;
+    int hebrew_size = 20;
+    int greek_size = 15;
+    int ipa_size = 15;
 };
 
 #endif
